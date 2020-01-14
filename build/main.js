94febf12c5eea5a1c83c4417ab887a5dfff5ff4e
Sebastian Bäumlisberger <s.baeumlisberger@live.de>
2020-01-14 11:16:26 +0100

    added build output

---
+++ build/main.js
@@ -0,0 +1,82 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// The adapter-core module gives you access to the core ioBroker functions
+const utils = require("@iobroker/adapter-core");
+class SIPAdapter extends utils.Adapter {
+    constructor(options = {}) {
+        super(Object.assign(Object.assign({}, options), { name: "sipadapter" }));
+        this.on("ready", this.onReady.bind(this));
+        this.on("objectChange", this.onObjectChange.bind(this));
+        this.on("stateChange", this.onStateChange.bind(this));
+        this.on("unload", this.onUnload.bind(this));
+    }
+    /**
+     * Is called when databases are connected and adapter received configuration.
+     */
+    onReady() {
+        return __awaiter(this, void 0, void 0, function* () {
+            this.log.info("start SIP Adapter");
+            yield this.setUpVariables();
+        });
+    }
+    setUpVariables() {
+        return __awaiter(this, void 0, void 0, function* () {
+            // in this template all states changes inside the adapters namespace are subscribed
+            this.subscribeStates("*");
+        });
+    }
+    /**
+     * Is called when adapter shuts down - callback has to be called under any circumstances!
+     */
+    onUnload(callback) {
+        try {
+            this.log.info("cleaned everything up...");
+        }
+        finally {
+            callback();
+        }
+    }
+    /**
+     * Is called if a subscribed object changes
+     */
+    onObjectChange(id, obj) {
+        if (obj) {
+            // The object was changed
+            this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
+        }
+        else {
+            // The object was deleted
+            this.log.info(`object ${id} deleted`);
+        }
+    }
+    /**
+     * Is called if a subscribed state changes
+     */
+    onStateChange(id, state) {
+        if (state) {
+            // The state was changed
+            this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
+        }
+        else {
+            // The state was deleted
+            this.log.info(`state ${id} deleted`);
+        }
+    }
+}
+if (module.parent) {
+    // Export the constructor in compact mode
+    module.exports = (options) => new SIPAdapter(options);
+}
+else {
+    // otherwise start the instance directly
+    (() => new SIPAdapter())();
+}
